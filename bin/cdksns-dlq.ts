eefb3b2991562ef120c822601607d22f68cab75e
suzukiken <suzukiken@gmail.com>
2021-04-18 01:19:44 +0000

    done

--- bin/cdksns-dlq.ts
+++ bin/cdksns-dlq.ts
@@ -3,11 +3,16 @@ import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { CdksnsDlqLambdaStack } from '../lib/cdksns-dlq-lambda-stack';
 import { CdksnsDlqSqsStack } from '../lib/cdksns-dlq-sqs-stack';
+import { CdksnsDlqSqsFifoStack } from '../lib/cdksns-dlq-sqs-fifo-stack';
+
 
 const app = new cdk.App();
 new CdksnsDlqLambdaStack(app, 'CdksnsDlqLambdaStack', {
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 });
 new CdksnsDlqSqsStack(app, 'CdksnsDlqSqsStack', {
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+});
+new CdksnsDlqSqsFifoStack(app, 'CdksnsDlqSqsFifoStack', {
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 });
---
+++ lib/cdksns-dlq-sqs-fifo-stack.ts
@@ -0,0 +1,49 @@
+import * as cdk from '@aws-cdk/core';
+import * as sqs from '@aws-cdk/aws-sqs';
+import * as sns from '@aws-cdk/aws-sns';
+import * as iam from '@aws-cdk/aws-iam';
+import * as subscriptions from '@aws-cdk/aws-sns-subscriptions';
+
+export class CdksnsDlqSqsFifoStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+    
+    const PREFIX_NAME = id.toLowerCase().replace("stack", "")
+
+    const topic = new sns.Topic(this, 'topic', {
+      topicName: PREFIX_NAME + '-topic',
+      fifo: true
+    })
+    
+    const queue = new sqs.Queue(this, 'queue', {
+      queueName: PREFIX_NAME + '-queue.fifo',
+      retentionPeriod: cdk.Duration.days(10),
+      fifo: true
+    })
+
+    topic.addSubscription(new subscriptions.SqsSubscription(queue))
+    
+    const dead_letter_queue = new sqs.Queue(this, 'dead_letter_queue', {
+      queueName: PREFIX_NAME + '-dead_letter_queue.fifo',
+      retentionPeriod: cdk.Duration.days(10),
+      fifo: true
+    })
+    
+    const statement = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      resources: [dead_letter_queue.queueArn],
+      actions: ['SQS:SendMessage'],
+      conditions: { ArnEquals: { 'aws:SourceArn': topic.topicArn } },
+      principals: [ new iam.ServicePrincipal('sns.amazonaws.com') ]
+    })
+    
+    dead_letter_queue.addToResourcePolicy(statement)
+    
+    new sns.Subscription(this, 'subscription', {
+      endpoint: queue.queueArn,
+      protocol: sns.SubscriptionProtocol.SQS,
+      topic,
+      deadLetterQueue: dead_letter_queue,
+    })
+  }
+}
